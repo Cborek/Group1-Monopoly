394334f943f804775f3e7de3528ff427a162b407
CooperLaredo <castle@student.neumont.edu>
2016-11-15 12:02:45 -0700

    Update UtilitySquare.java
    Removed int in getRent() as it is no longer needed. Works the same as RailRoadSquare Class

--- MonopolyGame/src/UtilitySquare.java
+++ MonopolyGame/src/UtilitySquare.java
@@ -15,7 +15,7 @@ public class UtilitySquare extends Square {
 		getSquareInfo();
 		System.out.println("This color is: " + getColor());
 		System.out.println("The cost is: " + getCost());
-		System.out.println("The rent is : " + getRent(1));
+		System.out.println("The rent is : " + getRent());
 		System.out.println("Rent with two owned: " + getRentWithTwo());
 		System.out.println("Mortgage value: " + getMortgage());
 	}
