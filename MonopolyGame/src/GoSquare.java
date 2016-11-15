73d48d098dd3972b4b1e41d76bd4f69a8256e81b
CooperLaredo <castle@student.neumont.edu>
2016-11-15 11:52:52 -0700

    Update GoSquare.java
    Remove int in getRent() as it is no longer needed.

--- MonopolyGame/src/GoSquare.java
+++ MonopolyGame/src/GoSquare.java
@@ -9,7 +9,7 @@ public class GoSquare extends Square{
 	
 	public void getInfo() {
 		getSquareInfo();
-		System.out.println("The rent is: " +getRent(0));
+		System.out.println("The rent is: " +getRent());
 	}
 	
 	//WRITE IN WHAT HAPPENS WHEN YOU LAND ON OR PASS THIS SQUARE
