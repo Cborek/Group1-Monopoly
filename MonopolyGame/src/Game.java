e738da2f7a1cddd0c04644eaad1610e0d09ef6fd
JWPeace <jcarpenter@student.neumont.edu>
2016-11-17 11:09:57 -0700

    Updates the game Auction method for Non Bought squares

--- MonopolyGame/src/Game.java
+++ MonopolyGame/src/Game.java
@@ -14,7 +14,7 @@ class Game {
 	//Start Menu for when application first starts up
 	private final String[] menu = {"Play a New Game", "Credits", "Exit"};
 	//options of the players turn
-	private final String[] playerOptions = {"Buy Space","Do not buy space","Sell property","Make offer","End turn","Quit game"};
+	private final String[] playerOptions = {"Buy Space","Do not buy space","Build","Sell property","Make offer","End turn","Quit game"};
 	//a list of the player
 	private ArrayList<Player> gameMembers = new ArrayList<Player>();
 	
@@ -48,7 +48,10 @@ class Game {
 			{
 				System.out.println (member.getName() + " is on " + gameArea.getPosName(member.getPlace()));
 			}
-			good = ConsoleUI.promptForBool("Is the game still being played? Y/N","y","n");
+			if(gameMembers.size()>1)
+			{
+				good = ConsoleUI.promptForBool("Is the game still being played? Y/N","y","n");
+			}		
 		}while(gameMembers.size()>1 && good);
 		if(gameMembers.size()==1)
 		{
@@ -78,6 +81,17 @@ class Game {
 				currentPlayer.setPlace(total);
 				System.out.println ("You are on "+gameArea.getPosName(currentPlayer.getPlace())+ " at space " +currentPlayer.getPlace());
 			}
+			//the position value is equal to the square the player is currently on
+			Square position = gameArea.getPos(currentPlayer.getPlace());
+			if(position.getIsOwned() && !currentPlayer.ownes(position))
+			{
+				System.out.println ("This space is owned you pay rent of $" + position.getRent() +" dollars");
+				currentPlayer.setMoney(-1*position.getRent());
+				gameMembers.get(position.getPlayer()-1).setMoney(position.getRent());
+				System.out.println (gameMembers.get(position.getPlayer()-1).getName() + " you have been paid rent");
+			}
+			System.out.println ();
+			System.out.println (currentPlayer.getName() + " it is your turn");
 			option = ConsoleUI.promptForMenuSelection(playerOptions,false);
 			if(gameArea.getPosName(currentPlayer.getPlace()).trim().equalsIgnoreCase("got to jail"))
 			{
@@ -85,22 +99,32 @@ class Game {
 				System.out.println ("you go to jail");
 				currentPlayer.goToPlace(11);
 			}
+			
 			if(option == 1)
 			{
-				if(!gameArea.getPos(currentPlayer.getPlace()).getIsOwned())
+				if(!position.getIsOwned() && currentPlayer.getMoney()>=position.getCost())
 				{
-					currentPlayer.setMoney(-1*gameArea.getPos(currentPlayer.getPlace()).getCost());
+					currentPlayer.setMoney(-1*position.getCost());
 					//Player purchases the space
-					currentPlayer.propertyChange(gameArea.getPos(currentPlayer.getPlace()));
+					currentPlayer.propertyChange(position);
 					// player is passed the square they are on
-					gameArea.getPos(currentPlayer.getPlace()).setIsOwned();
+					position.setIsOwned();
 					//the square is now owned;
+					position.setPlayer(gameMembers.indexOf(currentPlayer)+1);
 				}
-				
+				else
+				{
+					System.out.println ("You cannot buy that square");
+				}
 				//set a boolean within the square to say it is owned and by who
 				//check if player can afford it
 			}
-			if(doubles && turn!=3 && option == 5)
+			else if(option ==2)
+			{
+				//send current space to auction
+				auctionNotBought(position);
+			}
+			if(doubles && turn!=3 && option == 6)
 			{
 				System.out.println ("You rolled doubles go again");
 				turn++;
@@ -113,7 +137,18 @@ class Game {
 				//create the in jail or just visiting
 				doubles = false;
 			}
-		}while(option <5);
+		}while(option <6);
+		if(!gameArea.getPos(currentPlayer.getPlace()).getIsOwned() && option ==6)
+		{
+			auctionNotBought(gameArea.getPos(currentPlayer.getPlace()));
+		}
+		else if(option == 7)
+		{
+			System.out.println ("You have quit the game");
+			//action all property off
+			//remove player from list
+			gameMembers.remove(currentPlayer);
+		}
 	}
 	public int rollDice()
 	{
@@ -179,5 +214,59 @@ class Game {
 			a.add(rand.nextInt(a.size()), copy);
 		}
 		return a;
+	}
+	
+	//handles the auctions if a player does not buy the square they landed on
+	public void auctionNotBought(Square property)throws IOException
+	{
+		boolean isBought = false;
+		//use a player that holds the current highest bid
+		Player highBid = new Player("No one");
+		int bid =0;
+		//loops through players until bidding ceases
+		do
+		{
+			//control how many people are bidding
+			int bidding = gameMembers.size();
+			//loops through players offering them the chance to bid
+			for(int i=0; i<gameMembers.size();i++)
+			{
+				int attemptBid =0;
+				System.out.println (gameMembers.get(i).getName() + " The current bid is $" + bid +" and is held by "+ highBid.getName());
+				boolean isBidding = ConsoleUI.promptForBool("Would you like to place a bid? Y/N", "y","n");
+				if(isBidding && gameMembers.get(i).getMoney() > bid)
+				{
+					attemptBid = ConsoleUI.promptForInt("How much will you bid",bid+1,gameMembers.get(i).getMoney());
+				}
+				else
+				{
+					bidding--;
+				}
+				if(attemptBid > bid)
+				{
+					String checkBid = "You bid $" + attemptBid + ". Are you sure you want to bid? Y/N";
+					if(ConsoleUI.promptForBool(checkBid,"y","n"))
+					{
+						bid = attemptBid;
+						highBid=gameMembers.get(i);
+					}
+				}
+				else
+				{
+					System.out.println ("You are currently not bidding");
+				}
+			}
+			//when no one is bidding the auction is over
+			if(bidding==0)
+			{
+				isBought=true;
+			}
+		}while(!isBought || highBid.getName().equalsIgnoreCase("no one"));
+		System.out.println (highBid.getName() + " You have purchased " + property.getName() +" for $" + bid);
+		//give the winning player the space and retrive the money spent on the bid
+		property.setIsOwned();
+		property.setPlayer(gameMembers.indexOf(highBid)+1);
+		highBid.propertyChange(property);
+		highBid.setMoney(-1*bid);
 	}
 }
