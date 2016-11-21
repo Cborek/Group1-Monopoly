ca4392ab8732652e2534954b67d8a4d48a1867d7
melissabuena98 <Mbuena@student.neumont.edu>
2016-11-21 11:07:37 -0700

    Update Run.java

--- MonopolyGame/src/Run.java
+++ MonopolyGame/src/Run.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 public class Run {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("It's time to ruin friendships! LET'S PLAY MONOPOLY!");
 		Game playMonopoly = new Game();
 		playMonopoly.play();
 
