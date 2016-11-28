c10c1d401db1f85657bc9dbc8f2be520bb073cdf
CooperLaredo <castle@student.neumont.edu>
2016-11-28 15:19:14 -0700

    Add files via upload
    Added text to more clearly state what the get info is printing out.

--- MonopolyGame/src/UtilitySquare.java
+++ MonopolyGame/src/UtilitySquare.java
@@ -1,3 +1,5 @@
+package edu.neumont.csc110.a.monopoly;
+
 public class UtilitySquare extends Square {
 	
 	public UtilitySquare(int location, String name, String color, int cost, int rent, int rentWithTwo, int mortgage) {
@@ -13,8 +15,8 @@ public class UtilitySquare extends Square {
 		getSquareInfo();
 		System.out.println("This color is: " + getColor());
 		System.out.println("The cost is: " + getCost());
-		System.out.println("The rent is : " + getRent());
-		System.out.println("Rent with two owned: " + getRentWithTwo());
+		System.out.println("The rent is : " + getRent() +" * the amount rolled");
+		System.out.println("Rent with two owned is: " + getRentWithTwo() +" * the amount rolled");
 		System.out.println("Mortgage value: " + getMortgage());
 	}
 }
