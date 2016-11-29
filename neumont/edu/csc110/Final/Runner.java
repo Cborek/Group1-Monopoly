4e807808838968b8026313824a1a7abffcc6991a
Cborek <CBorek@student.neumont.edu>
2016-11-29 11:39:51 -0700

    Delete Runner.java

--- neumont/edu/csc110/Final/Runner.java
+++
@@ -1,9 +0,0 @@
-package neumont.edu.csc110.Final;
-import java.io.IOException;
-
-class Runner {
-	public static void main(String[] args)throws IOException {
-		//Game myGame = new Game();
-		//myGame.startMenu();
-	}	
-}
